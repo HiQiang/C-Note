bdf8a78fad961e4dcc97e201404876b3651a98eb
强 <1677412161@qq.com>
2021-09-16 12:15:48 +0800

    Update 读文件.cpp

--- 读写文件/读文件.cpp
+++ 读写文件/读文件.cpp
@@ -10,19 +10,20 @@ void test01()
 	ifstream ifs;
 	//3.打开文件，并且判定是否打开成功
 	ifs.open("text.txt", ios::in);
-
 	if (!ifs.is_open()) {
 		cout << "文件打开失败" << endl;
 		return;
 	};
 
 	//4.读数据 4种方式
-	//char buf[1024] = { 0 };
-	//while (ifs >> buf)
-	//{
-	//	cout << buf << endl;
-	//}
-	//
+
+	/*char buf[1024] = { 0 };
+	while (ifs >> buf)
+	{
+		cout << buf << endl;
+	}
+	ifs.close();
+	*/
 
 	/*char buf[1024] = { 0 };
 	while (ifs.getline(buf, sizeof(buf))) 
@@ -30,10 +31,13 @@ void test01()
 		cout << buf << endl;
 	}
 	ifs.close();*/
-	string buf;
+
+	/*string buf;
 	while (getline(ifs, buf)) {
 		cout << buf << endl;
 	}
+	ifs.close();*/
+
 	char c;
 	while ((c = ifs.get() != EOF))
 	{
