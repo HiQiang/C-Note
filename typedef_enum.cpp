27cf1845187d2e31278ec45979ef747d4ae36da9
强 <1677412161@qq.com>
2021-09-25 22:36:15 +0800

    Create typedef_enum.cpp

---
+++ typedef_enum.cpp
@@ -0,0 +1,15 @@
+typedef int INTNUM;
+//typedef 数据类型 类型别名
+//typedef 关键字，使用它来为类型取一个新的名字
+
+typedef enum tagDay{MON = 1} Day;
+
+// enum tagDay{MON = 1} //定义了一个自定义的数据类型，和 int 一样是属于数据类型，只不过是自定义的，此处的 Day 和 INTNUM相同，属于为数据类型定义的别名
+// 以后 可以直接用 Day 声明变量
+Day day1;
+day1 = MON;
+
+//上述用法和下述用法效果一样
+enum Day{MON = 1};
+Day day1;
+day1 = MON;
