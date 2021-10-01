e0e49d063984a196052fb201cbb1f5ad7946e0e0
强 <1677412161@qq.com>
2021-10-01 20:30:02 +0800

    Create deque大小操作.cpp

---
+++ 初学STL/deque大小操作.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<deque>
+
+using namespace std;
+
+//deque 容器的大小进行操作
+
+void printDeque(const deque<int>& d)
+{
+	for (deque<int>::const_iterator it = d.begin(); it != d.end(); it++)
+	{
+		cout << (*it) << " ";
+	}
+	cout << endl;
+}
+
+void test01()
+{
+	deque<int>d1;
+	for (int i = 0; i < 10; i++)
+	{
+		d1.push_back(i);
+	}
+	printDeque(d1);
+
+	if (d1.empty())
+	{
+		cout << "d1为空" << endl;
+	}
+	else
+	{
+		cout << "d1不为空" << endl;
+		cout << d1.size() << endl;
+		//deque容器没有容量的概念
+	}
+
+	//重新指定大小
+	d1.resize(15,100);//默认0填充
+	printDeque(d1);
+
+	d1.resize(5);
+	printDeque(d1);
+	
+}
+
+
+int main()
+{
+	test01();
+	return 0;
+}
